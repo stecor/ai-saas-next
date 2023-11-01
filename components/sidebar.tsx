f0a0b1cb924ec66974cb60524f0d5cca700c34fb
Stefano Corra <stefano.corra@hotmail.com>
2023-10-31 20:53:18 -0400

    sidebar conversation commit

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,6 +6,7 @@ import { Montserrat } from "next/font/google"
 
 import { cn } from "@/lib/utils"
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, MusicIcon, Settings, VideoIcon } from "lucide-react"
+import { usePathname } from "next/navigation"
 
 const montserrat = Montserrat({
   weight: '600',
@@ -57,6 +58,7 @@ const routes = [
 ]
 
 const Sidebar = () => {
+  const pathname = usePathname()
   return (
       <div className="space-y-4 py-4 flex 
     flex-col h-full bg-[#111827] text-white">
@@ -76,8 +78,10 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer
-               hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(`text-sm group flex p-3 w-full 
+              justify-start font-medium cursor-pointer
+               hover:text-white hover:bg-white/10 rounded-lg transition`,
+                pathname === route.href ? "text-white bg-white/10": "text-zinc-400")}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
