268b18a0dc75efdf53be84cec5fe12a37a1ad75b
Stefano Corra <stefano.corra@hotmail.com>
2023-11-01 22:06:27 -0400

    sidebar conversation commit

--- app/(dashboards)/(routes)/dashboard/page.tsx
+++ app/(dashboards)/(routes)/dashboard/page.tsx
@@ -2,7 +2,8 @@
 
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { ArrowRight, MessageSquare } from "lucide-react";
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 
 const tools = [{
@@ -11,9 +12,38 @@ const tools = [{
   color: 'text-violet-500',
   bgColor: 'bg-violet-500/10',
   href: '/conversation'
-}]
+},
+{
+  label: 'Music Generation',
+  icon: Music,
+  color: 'text-emerald-500',
+  bgColor: 'bg-emerald-500/10',
+  href: '/music'
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: 'text-pink-700',
+    bgColor: 'bg-pink-700/10',
+    href: '/image'
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: 'text-orange-700',
+    bgColor: 'bg-orange-700/10',
+    href: '/video'
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: 'text-green-700',
+    bgColor: 'bg-green-700/10',
+    href: '/code'
+  },]
 
 const DashboardPage = () => {
+  const router = useRouter()
   return (
     <div className="mb-8 space-y-4">
       <h2 className="text-2xl md:text-4xl font-bold text-center">
@@ -25,6 +55,7 @@ const DashboardPage = () => {
       <div className="px-4 md:px-20 lg:px32 space-y-4">
         {tools.map((tool) => (
           <Card
+            onClick={()=> router.push(tool.href)}
             key={tool.href}
             className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
           >
