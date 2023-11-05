f3ab8f12097032edc5ce6f81f2937510f6d2a109
Stefano Corra <stefano.corra@hotmail.com>
2023-11-05 16:20:07 -0500

    free trial limit commit

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from 'replicate'
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api_limit";
+
 
 
 const replicate = new Replicate({
@@ -24,7 +26,13 @@ export async function POST(
 
         if (!prompt) {
             return new NextResponse('Prompt is required',{status:400})
-        }
+      }
+      
+      const freeTrial = await checkApiLimit()
+
+      if (!freeTrial) {
+        return new NextResponse('Free trial has expired.',{status:403})
+      }
 
         const response = await replicate.run(
           "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
@@ -35,6 +43,7 @@ export async function POST(
           }
         );
       
+      await increaseApiLimit()
 
         return NextResponse.json(response)
 
