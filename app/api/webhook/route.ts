7a6c577086db0b5f84bf67ed128021410a4eee97
Stefano Corra <stefano.corra@hotmail.com>
2023-11-15 20:20:24 -0500

    date stripe fix commit

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -9,7 +9,8 @@ import Stripe from "stripe"
 
 
 
-export async function POST(req:Request) {
+export async function POST(req: Request) {
+  
     const body = await req.text()
     const signature = headers().get("Stripe-Signature") as string
 
