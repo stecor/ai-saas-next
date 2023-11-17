16a67dace449cd785880633ced23c3202f0ef791
Stefano Corra <stefano.corra@hotmail.com>
2023-11-16 21:57:30 -0500

    pro-modal fix commit

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 import { Montserrat } from "next/font/google"
 
 import { cn } from "@/lib/utils"
-import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, MusicIcon, Settings, VideoIcon } from "lucide-react"
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react"
 import { usePathname } from "next/navigation"
 import FreeCounter from "@/components/free-counter"
 
