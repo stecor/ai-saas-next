765da414bb575c8d710d28ff71a988b8fac94055
Stefano Corra <stefano.corra@hotmail.com>
2023-11-17 08:14:28 -0500

    pro-modal fix commit

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,59 +1,51 @@
-
-import { headers } from "next/headers"
-import { stripe } from "@/lib/stripe"
-import { NextResponse } from "next/server"
-import prismadb from "@/lib/prismadb"
 import Stripe from "stripe"
+import { headers } from "next/headers"
+import { NextResponse } from "next/server"
 
-
-
-
+import prismadb from "@/lib/prismadb"
+import { stripe } from "@/lib/stripe"
 
 export async function POST(req: Request) {
-  
-    const body = await req.text()
-    const signature = headers().get("Stripe-Signature") as string
+  const body = await req.text()
+  const signature = headers().get("Stripe-Signature") as string
 
-    let event: Stripe.Event
+  let event: Stripe.Event
 
-    try {
-        event = stripe.webhooks.constructEvent(
-            body,
-            signature,
-            process.env.STRIPE_WEBHOOK_SECRET!
-        )
-        
-    } catch (error:any) {
-        return new NextResponse(`Webhook Error: ${error.message}`,{status:400})
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    )
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session
+
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    )
+
+    if (!session?.metadata?.userId) {
+      return new NextResponse("User id is required", { status: 400 });
     }
 
-    const session = event.data.object as Stripe.Checkout.Session
-
-    if (event.type === "checkout.session.completed") {
-        const subscription = await stripe.subscriptions.retrieve(
-            session.subscription as string
-        )
-
-        if (!session?.metadata?.userId) {
-            return new NextResponse("User id is required", {status:400})
-        }
-
-        await prismadb.userSubscription.create({
-            data: {
-                userId: session?.metadata?.userId,
-                stripeSubscriptionId: subscription.id,
-                stripeCustomerId: subscription.customer as string,
-                stripePriceId: subscription.items.data[0].price.id,
-                stripeCurrentPeriodEnd: new Date(
-                subscription.current_period_end * 1000
-                )
-
-            }
-        })
+    await prismadb.userSubscription.create({
+      data: {
+        userId: session?.metadata?.userId,
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer as string,
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    })
   }
-  
-  console.log("webhook/route.ts - event.type"+event.type);
-          if (event.type === "invoice.payment_succeeded") {
+
+  if (event.type === "invoice.payment_succeeded") {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     )
@@ -72,6 +64,4 @@ export async function POST(req: Request) {
   }
 
   return new NextResponse(null, { status: 200 })
-}
-
-
+};
