ef1d932fc3f7ef670a5ab4036b7443dc9a1ff933
Stefano Corra <stefano.corra@hotmail.com>
2023-11-19 14:27:09 -0500

    fix landing page commit

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -25,12 +25,12 @@ export const LandingNavbar = () => {
       </Link>
       <div className="md:flex md:items-center gap-x-2 p-8">
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button variant="outline" className="md:absolute rounded-full md:-ml-16 px-7 -mt-5 z-50 mr-2 bg-[#E449A3] border-none hover:bg-[#f082b1]" >
+          <Button variant="outline" className="rounded-full  px-10 -mt-5  ml-20 bg-[#E449A3] border-none hover:bg-[#f082b1]" >
           Sign-up
           </Button>
         </Link>
         <Link href={isSignedIn ? "/dashboard" : "/sign-in"} >
-          <Button variant="outline" className="md:float-left rounded-[50px] px-10 md:mr-6 z-0 bg-[#885DF4] border-none hover:bg-[#71297a]">
+          <Button variant="outline" className=" rounded-full px-10 md:mr-0  bg-[#885DF4] border-none hover:bg-[#71297a]">
           Sign-in
           </Button>
         </Link>
