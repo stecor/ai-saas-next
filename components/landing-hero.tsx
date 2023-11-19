8c25a8b6011f46bfe00a81655680edd2e8879e1d
Stefano Corra <stefano.corra@hotmail.com>
2023-11-18 22:11:25 -0500

    fix landing page commit

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,7 +12,7 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-10 text-center space-y-3">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>The Best AI Tool for</h1>
+        <h2>The Best AI Tool for</h2>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 pb-2">
           <TypewriterComponent
             options={{
--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -14,10 +14,10 @@ export const LandingNavbar = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <nav className="p-4 bg-transparent md:flex md:items-center md:justify-between text-center ">
+    <nav className="p-4 bg-transparent md:flex md:items-center md:justify-between ">
       <Link href="/" className="flex items-center">
-        <div className="relative h-8 w-8 mr-4">
-          <Image fill alt="Logo" src="/logo.png" sizes="h-8 w-8" className="sm:m-auto"/>
+        <div className="relative h-8 w-8 mr-4 ">
+          <Image fill alt="Logo" src="/logo.png" sizes="h-8 w-8" />
         </div>
         <h1 className={cn("text-2xl font-bold text-white", font.className)}>
           Creator AI
@@ -25,7 +25,7 @@ export const LandingNavbar = () => {
       </Link>
       <div className="md:flex md:items-center gap-x-2 p-8">
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button variant="outline" className="md:absolute rounded-full md:-ml-16 px-7 -mt-5 z-50 mr-4 bg-[#E449A3] border-none hover:bg-[#f082b1]" >
+          <Button variant="outline" className="md:absolute rounded-full md:-ml-16 px-7 -mt-5 z-50 mr-2 bg-[#E449A3] border-none hover:bg-[#f082b1]" >
           Sign-up
           </Button>
         </Link>
