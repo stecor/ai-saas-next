a11835d92caba489feea5288004b24d87b0d8bb0
Stefano Corra <stefano.corra@hotmail.com>
2023-11-18 22:00:07 -0500

    fix landing page commit

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -14,7 +14,7 @@ export const LandingNavbar = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <nav className="p-4 bg-transparent flex items-center justify-between">
+    <nav className="p-4 bg-transparent md:flex md:items-center md:justify-between text-center ">
       <Link href="/" className="flex items-center">
         <div className="relative h-8 w-8 mr-4">
           <Image fill alt="Logo" src="/logo.png" sizes="h-8 w-8" className="sm:m-auto"/>
@@ -23,14 +23,14 @@ export const LandingNavbar = () => {
           Creator AI
         </h1>
       </Link>
-      <div className="flex items-center gap-x-2 ">
+      <div className="md:flex md:items-center gap-x-2 p-8">
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button variant="outline" className="absolute rounded-full -ml-16 px-7 -mt-5 z-50 bg-[#E449A3] border-none hover:bg-[#f082b1]" >
+          <Button variant="outline" className="md:absolute rounded-full md:-ml-16 px-7 -mt-5 z-50 mr-4 bg-[#E449A3] border-none hover:bg-[#f082b1]" >
           Sign-up
           </Button>
         </Link>
         <Link href={isSignedIn ? "/dashboard" : "/sign-in"} >
-          <Button variant="outline" className="float-left rounded-[50px] px-10 mr-6 z-0 bg-[#885DF4] border-none hover:bg-[#71297a]">
+          <Button variant="outline" className="md:float-left rounded-[50px] px-10 md:mr-6 z-0 bg-[#885DF4] border-none hover:bg-[#71297a]">
           Sign-in
           </Button>
         </Link>
