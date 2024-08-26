4f186a880d07b074165dd657897f5cac242dabfd
Stefano Corra <stefano.corra@hotmail.com>
2024-08-26 17:58:40 -0400

    Fixed gtm

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,13 @@
 import { LandingNavbar } from "@/components/landing-navbar";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingContent } from "@/components/landing-content";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const LandingPage = () => {
   return ( 
+    
     <div className="h-full ">
+       <GoogleAnalytics gaId={`${process.env.GA_TRACKING_ID}`} />
       <LandingNavbar />
       <LandingHero />
       <LandingContent />
