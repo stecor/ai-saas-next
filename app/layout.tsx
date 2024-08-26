b68f2bfa7085ead45679702bafcf1195186353ee
Stefano Corra <stefano.corra@hotmail.com>
2024-08-26 14:22:03 -0400

    Fixed seo elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Chat-GPT Creator',
   description: 'The power of artificial intelligence to revolutionize the way businesses and individuals interact with technology. Our cutting-edge AI solutions provide automation, predictive insights, and intelligent decision.',
+
 }
 
 export default function RootLayout({
@@ -20,11 +21,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+   
     <ClerkProvider>
+   
       <html lang="en">
+      <head>
+          <link rel="icon"  type="image/x-icon" href="/logo.png" />
+          <title>Chat-GPT Creator AI Platform</title>
+      </head>
         <CrispProvider/>
         <body className={inter.className}>
-          <title>Chat-GPT Creator AI Platform</title>
+          
+          
           <ModalProvider />
           <ToasterProvider/>
           {children}
