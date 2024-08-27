2c71537c09a24c7b7bc23d31ccc45ccc66c005c2
Stefano Corra <stefano.corra@hotmail.com>
2024-08-27 12:22:42 -0400

    Fixing path settings

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -19,7 +19,8 @@ export async function POST(req: Request) {
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     )
-  } catch (error: any) {
+  } catch (error: any)
+  {
     return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
   }
 
