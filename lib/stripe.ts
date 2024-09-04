f848b42690b2be33627766f9a288ab8c4af4b2fc
Stefano Corra <stefano.corra@hotmail.com>
2024-09-04 12:27:56 -0400

    Fixing stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,6 +2,7 @@ import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY || "",
     {
-    apiVersion: "2023-10-16",
-    typescript: true,
-})
+        apiVersion: "2022-11-15",
+        typescript: true,
+    })
+
