4509681b55e595f5c165884115929a2531f0cbd4
Stefano Corra <stefano.corra@hotmail.com>
2024-09-06 09:08:39 -0400

    Fixing signIn/signOut

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -25,7 +25,7 @@ export const LandingNavbar = () => {
         </h1>
       </Link>
       <div className="flex items-center gap-x-2">
-        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+        <Link href={isSignedIn ? "/dashboard" : "/sign-in"}>
           <Button variant="outline" className="rounded-full">
             Sign In
           </Button>
