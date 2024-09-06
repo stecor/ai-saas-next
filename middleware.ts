88c631c405afb07665d953c8057e19b793659df5
Stefano Corra <stefano.corra@hotmail.com>
2024-09-06 08:12:03 -0400

    Fixing sign

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,32 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
-authMiddleware({ debug: true })
+import { NextResponse } from "next/server";
+
+export const runtime = "nodejs";
 
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhook', ]
+  publicRoutes: [
+    "/",
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/api(.*)",
+  ],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      //  For public routes, we don't need to do anything
+      return NextResponse.next();
+    }
+
+    const url = new URL(req.nextUrl.origin);
+
+    if (!auth.userId) {
+      //  If user tries to access a private route without being authenticated,
+      //  redirect them to the sign in page
+      url.pathname = "/sign-in";
+      return NextResponse.redirect(url);
+    }
+  },
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)"],
 };
